55270cdd71e44db1d1e75b2a3775fd75cb766aff
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-03-26 18:40:57 +0530

    Time: 29 ms (64.5%), Space: 33.5 MB (88.18%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+
+    // vector<int>twoPtrMethod(vector<int> & arr, int k , int x){
+    //     int l=0,h=arr.size()-1;
+
+    //     while(h-l >=k){
+    //         if(x-arr[l] > arr[h]-x){
+    //             l++;
+    //         }
+    //         else{
+    //             h--;
+    //         }
+    //     }
+    //     vector<int> ans;
+    //     for(int i=l; i<=h; i++){
+    //         ans.push_back(arr[i]);
+    //     }
+    //     return ans;
+    // }
+
+    int lowerBound(vector<int> &arr, int x){
+        int s=0, e=arr.size()-1;
+        int mid= s+(e-s)/2;
+        int ans=e;
+        while(s<=e){
+            if(x<=arr[mid]){
+                ans=mid;
+                e=mid-1;
+            }
+            else if(x>arr[mid]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+            mid= s+(e-s)/2;
+        }
+        return ans;
+    }
+
+    vector<int> bs_method(vector<int>& arr, int k, int x){
+        int h=lowerBound(arr,x);
+        int l=h-1;
+
+        while(k--){
+            if(l<0){
+                h++;
+            }
+            else if(h>=arr.size()){
+                l--;
+            }
+            else if(x-arr[l]> arr[h]-x){ //comparing the difference
+                h++;
+            }
+            else{
+                l--;
+            }
+        }
+        return vector<int> (arr.begin() + l + 1, arr.begin() + h);
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        //return twoPtrMethod(arr, k, x);
+        return bs_method(arr, k, x);
+    }
+};
