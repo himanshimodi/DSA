326c331fe9f1fa773f4c8f37eb6e280adcc40c1c
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-04-06 12:06:33 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.75%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substringruntime : 0ms.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int pos= s.find(part);
+        while(pos!=string::npos){
+            // staring index se leke 'part' ke length tak
+            s.erase(pos,part.length()); 
+            //update while loop ka vriable
+            pos=s.find(part);
+        }
+        return s;
+    }
+};
