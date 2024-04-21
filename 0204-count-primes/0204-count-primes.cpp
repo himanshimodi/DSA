eca692b3673a616e8b82d21468f505e5d0df704f
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-04-21 19:26:31 +0530

    Time: 523 ms (31.78%), Space: 11.5 MB (65.13%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -5,13 +5,18 @@ public:
 
         vector<bool> prime(n, true); //all are marked as prime in the start
         prime[0] = prime[1] = false; // 0 and 1 are always marked as false as they are not prime
-        
-        int ans=0;
+
+        int ans=0; // this is a counter variable
 
         for(int i=2; i<n; i++){
-            if((i%2)==0){
-                prime[i] = false; // i is in the table of 2
+            if(prime[i]){
                 ans++;
+
+                int j=2*i;
+                while(j<n){
+                    prime[j] = false;
+                    j=j+i; // j is multiples of i, so all mutiples of i are marked as false
+                }
             }
         }        
         return ans;
