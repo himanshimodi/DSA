206dfa240552246059213f1ddc8879bce8384533
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-06-07 17:05:22 +0530

    Time: 4 ms (60.92%), Space: 11.8 MB (25.91%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,77 +1,76 @@
 class Solution {
 public:
     
-unordered_map <int, bool> rowCheck;
-unordered_map <int, bool> upperLeftDiagonalCheck;
-unordered_map <int, bool> bottomLeftDiagonalCheck;
+    unordered_map <int, bool> rowCheck;
+    unordered_map <int, bool> upperLeftDiagonalCheck;
+    unordered_map <int, bool> bottomLeftDiagonalCheck;
 
-void storeSolution( vector<vector<char>> & board, int n, vector<vector<string>> &ans){
-	vector<string> temp;
-	for(int i=0; i<n; i++){
-		string output="";
-		for(int j=0; j<n; j++){
-			output.push_back(board[i][j]);
-                }
-	        temp.push_back(output);
-	}
-	ans.push_back(temp);
-}
+    void storeSolution( vector<vector<char>> & board, int n, vector<vector<string>> &ans){
+        vector<string> temp;
+        for(int i=0; i<n; i++){
+            string output="";
+            for(int j=0; j<n; j++){
+                output.push_back(board[i][j]);
+                    }
+                temp.push_back(output);
+        }
+        ans.push_back(temp);
+    }
 
-bool isSafe(int row, int col, vector<vector<char>> &board, int n){
-	
-	//check left row
-	if(rowCheck[row] == true)
-	   return false;
-	
-	//check upper Left Diagonal
-	if(upperLeftDiagonalCheck[n-1+col-row] == true)
-	   return false;
+    bool isSafe(int row, int col, vector<vector<char>> &board, int n){
+        
+        //check left row
+        if(rowCheck[row] == true)
+        return false;
+        
+        //check upper Left Diagonal
+        if(upperLeftDiagonalCheck[n-1+col-row] == true)
+        return false;
 
-	//check bottom left Diagonal 
-	if(bottomLeftDiagonalCheck[row+col] == true)
-	   return false;
-	
-	//else
-	return true;
-}
+        //check bottom left Diagonal 
+        if(bottomLeftDiagonalCheck[row+col] == true)
+        return false;
+        
+        //else
+        return true;
+    }
 
-void solve(vector<vector<char>> &board, int col, int n, vector<vector<string>> &ans){
+    void solve(vector<vector<char>> &board, int col, int n, vector<vector<string>> &ans){
 
-	//base case
-	if(col >= n){
-	  storeSolution(board, n, ans);
-	  return;
-	}
+        //base case
+        if(col >= n){
+        storeSolution(board, n, ans);
+        return;
+        }
 
-	//ek case solve krdo baki recursion sambhal lega
-	for(int row=0; row<n; row++){
-	   if(isSafe(row, col, board, n)){
-	     //place the Queen
-	     board[row][col] = 'Q';
-	     rowCheck[row] = true;
-	     upperLeftDiagonalCheck[n-1+col-row] = true;
-	     bottomLeftDiagonalCheck[row+col] = true;
-		
-	     //recursion
-	     solve(board, col+1, n, ans);
+        //ek case solve krdo baki recursion sambhal lega
+        for(int row=0; row<n; row++){
+        if(isSafe(row, col, board, n)){
+            //place the Queen
+            board[row][col] = 'Q';
+            rowCheck[row] = true;
+            upperLeftDiagonalCheck[n-1+col-row] = true;
+            bottomLeftDiagonalCheck[row+col] = true;
+            
+            //recursion
+            solve(board, col+1, n, ans);
 
-	     //backtracking
-	     board[row][col] = '.';
-	     rowCheck[row] = false;
-	     upperLeftDiagonalCheck[n-1+col-row] = false;
-	     bottomLeftDiagonalCheck[row+col] = false;
-	   }
-	}
-}
+            //backtracking
+            board[row][col] = '.';
+            rowCheck[row] = false;
+            upperLeftDiagonalCheck[n-1+col-row] = false;
+            bottomLeftDiagonalCheck[row+col] = false;
+        }
+        }
+    }
 
-vector<vector<string>> solveNQueens(int n){
-	vector<vector<char>> board (n, vector<char>(n,'.'));
-	vector<vector<string>> ans;
-	int col=0;
-	solve(board, col, n, ans);
+    vector<vector<string>> solveNQueens(int n){
+        vector<vector<char>> board (n, vector<char>(n,'.'));
+        vector<vector<string>> ans;
+        int col=0;
+        solve(board, col, n, ans);
 
-	return ans;
-}
-	
+        return ans;
+    }	
 };
 
