37f939cdc007c5db6850302d6027443e53e3f434
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-07-15 00:16:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (25.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+
+            // opening bracket
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                // closing bracket (ch is of input string)
+                if (!st.empty()) {
+                    char topCh = st.top();
+
+                    // matching brackets
+                    if (ch == ')' && topCh == '(') {
+                        st.pop();
+                    } else if (ch == '}' && topCh == '{') {
+                        st.pop();
+                    } else if (ch == ']' && topCh == '[') {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if (st.empty()) {
+            // valid
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
