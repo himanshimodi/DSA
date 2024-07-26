bbbb5f26e12e95ffe7ab6e092454c955120538eb
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-07-26 23:06:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First non-repeating character in a stream/first-nonrepeating-character-in-a-stream.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    int freq[26]={0};
+		    queue<char>q;
+		    
+		    string ans="";
+		    
+		    for(int i=0; i<A.length(); i++){
+		        char ch = A[i];
+		        
+		        //increment frequency
+		        freq[ch-'a']++;
+		        q.push(ch);
+
+    	        while(!q.empty()){
+    	            if(freq[q.front()-'a']>1){
+    	                q.pop();
+    	            }
+    	            else{
+    	                ans.push_back(q.front());
+    	                break;
+    	            }
+    	        }
+		    
+    	        if(q.empty()){
+    	            ans.push_back('#');
+    	        }
+		    }
+		    
+		    return ans;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
