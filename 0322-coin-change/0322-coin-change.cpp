00cf5f4bd67ea1d21ee20c097d34cbe2d2f63f26
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-09-08 19:41:12 +0530

    Time: 26 ms (72.41%), Space: 16.4 MB (85.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -22,22 +22,47 @@ public:
     //     return mini;
     // }
 
-    int solveUsingMemo(vector<int>& coins, int amount, vector<int> &dp){
-        //base case
-        if (amount == 0) {
-            return 0;
-        }
+    // int solveUsingMemo(vector<int>& coins, int amount, vector<int> &dp){
+    //     //base case
+    //     if (amount == 0) {
+    //         return 0;
+    //     }
 
-        //step 3: if already stored
-        if(dp[amount]!= -1){
-            return dp[amount];
-        }
+    //     //step 3: if already stored
+    //     if(dp[amount]!= -1){
+    //         return dp[amount];
+    //     }
 
-        // step 4: RE logic
-        int mini = INT_MAX;
-        for (int i = 0; i < coins.size(); i++) {
-            if (amount - coins[i] >= 0) {
-                int ans = solveUsingMemo(coins, amount - coins[i], dp);
+    //     // step 4: RE logic
+    //     int mini = INT_MAX;
+    //     for (int i = 0; i < coins.size(); i++) {
+    //         if (amount - coins[i] >= 0) {
+    //             int ans = solveUsingMemo(coins, amount - coins[i], dp);
+    //             // matlab valid ans hai
+    //             if (ans != INT_MAX) {
+    //                 int finalAns = 1 + ans;
+    //                 mini = min(mini, finalAns);
+    //             }
+    //         }
+    //     }
+    //     //step 2
+    //     dp[amount] = mini;
+    //     return dp[amount];
+    // }
+
+    int solvedUsingTabulation(vector<int>& coins, int amount){
+        //step 1: create a dp
+        int n = amount;
+        vector<int> dp (n+1, INT_MAX);
+
+        //step2 - base case analyse
+        dp[0] = 0;
+
+        for(int val=1; val<=amount; val++){
+            int mini = INT_MAX;
+            for (int i = 0; i < coins.size(); i++) {
+            if (val - coins[i] >= 0) {
+                int ans = dp[val - coins[i]];
                 // matlab valid ans hai
                 if (ans != INT_MAX) {
                     int finalAns = 1 + ans;
@@ -46,8 +71,9 @@ public:
             }
         }
         //step 2
-        dp[amount] = mini;
-        return dp[amount];
+        dp[val] = mini;
+        }
+        return dp[amount]; 
     }
 
     int coinChange(vector<int>& coins, int amount) {
@@ -60,10 +86,10 @@ public:
         // }
 
         //step 1: create a dp
-        int n = amount;
-        vector<int> dp (n+1, -1);
+        // int n = amount;
+        // vector<int> dp (n+1, -1);
 
-        int ansHere = solveUsingMemo(coins, amount, dp);
+        int ansHere = solvedUsingTabulation(coins, amount);
         if(ansHere == INT_MAX){
             return -1;
         }
