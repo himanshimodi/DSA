067fe4ef4f28f86a3d24d0bd11314e3576eec1b6
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-09-14 01:44:31 +0530

    Time: 0 ms (100%), Space: 12.9 MB (41.96%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -26,10 +26,46 @@ public:
         return ans;
     }
 
+    int solveUsingMem(string &a, string &b, int i, int j, vector<vector<int>>&dp){
+        //base case
+        if(j == b.length()){
+            //number of deletions
+            return a.length() - i;
+        }
+        if(i == a.length()){
+            return b.length() - j;
+        }
+
+        //check if ans already exists
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int ans = 0;
+        //matching characters
+        if(a[i] == b[j]){
+            ans = 0 + solveUsingMem(a,b,i+1,j+1,dp);
+        }
+        //not matching characters
+        else{
+            int replace = 1 + solveUsingMem(a,b,i+1,j+1,dp);
+            int insert = 1 + solveUsingMem(a,b,i,j+1,dp);
+            int remove = 1 + solveUsingMem(a,b,i+1,j,dp);
+            ans = min(insert,min(remove,replace));
+        }
+        //store ans
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
+
     int minDistance(string word1, string word2) {
         int i=0;
         int j=0;
-        int ans = solveUsingRe(word1,word2,i,j);
+        // int ans = solveUsingRe(word1,word2,i,j);
+        // return ans;
+
+        vector<vector<int>> dp (word1.length()+1,vector<int>(word2.length()+1, -1));
+        int ans = solveUsingMem(word1,word2,i,j,dp);
         return ans;
     }
 };
