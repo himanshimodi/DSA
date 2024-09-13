15c2222b8e5058613cc0b49d8e08eb355681e960
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-09-14 01:44:43 +0530

    Time: 10 ms (42.6%), Space: 12.9 MB (41.96%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -58,14 +58,52 @@ public:
         return dp[i][j];
     }
 
+    int solveUsingTab(string &a, string &b ){
+
+        //step 1 : create a dp array
+        vector<vector<int>> dp (a.length()+1,vector<int>(b.length()+1, -1));
+
+        //analyse base cases
+        for(int col=0; col<=b.length(); col++){
+            dp[a.length()][col] = b.length()-col;
+        }
+        for(int row=0; row<=a.length(); row++){
+            dp[row][b.length()] = a.length()-row;
+        }
+
+        //Re logic
+        for(int i=a.length()-1; i>=0; i--){
+            for(int j=b.length()-1;j>=0; j--){
+                int ans = 0;
+                //matching characters
+                if(a[i] == b[j]){
+                ans = 0 + dp[i+1][j+1];
+                }
+                //not matching characters
+                else{
+                int replace = 1 + dp[i+1][j+1];
+                int insert = 1 + dp[i][j+1];
+                int remove = 1 + dp[i+1][j];
+                ans = min(insert,min(remove,replace));
+                }
+                //store ans
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+
     int minDistance(string word1, string word2) {
         int i=0;
         int j=0;
         // int ans = solveUsingRe(word1,word2,i,j);
         // return ans;
 
-        vector<vector<int>> dp (word1.length()+1,vector<int>(word2.length()+1, -1));
-        int ans = solveUsingMem(word1,word2,i,j,dp);
+        // vector<vector<int>> dp (word1.length()+1,vector<int>(word2.length()+1, -1));
+        // int ans = solveUsingMem(word1,word2,i,j,dp);
+        // return ans;
+
+        int ans = solveUsingTab(word1,word2);
         return ans;
     }
 };
