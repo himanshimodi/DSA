5f9176c19f2ffd7a5299ceff7f3036452eba3406
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-10-02 17:36:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -49,6 +49,30 @@ public:
         return dp[i][j];
     }
 
+    int solveUsingTab(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][m] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                long long ans = 0;
+                if (s[i] == t[j]) {
+                    ans += dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    ans += dp[i + 1][j];
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
@@ -58,8 +82,11 @@ public:
         // int ans = solveUsingRe(s, t, n, m, i, j);
         // return ans;
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        int ans = solveUsingMem(s, t, n, m, i, j, dp);
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // int ans = solveUsingMem(s, t, n, m, i, j, dp);
+        // return ans;
+
+        int ans = solveUsingTab(s, t);
         return ans;
     }
 };
