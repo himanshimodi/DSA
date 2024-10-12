bb6990571aba82013a9381309f47d3e2dcea859f
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-10-12 21:57:42 +0530

    Time: 24 ms (36.53%), Space: 19.8 MB (34.06%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(int src, unordered_map<int,bool>&vis, int n, vector<vector<int>>& isConnected){
+        vis[src]=true;
+
+        //node row ke nbr nikalo
+        int col=n;
+        int row=src;
+        for(int nbrIndex=0; nbrIndex<col; nbrIndex++){
+            int nbr = nbrIndex;
+            if(src!=nbr && isConnected[row][nbrIndex]==1){
+                if(vis[nbr]!=true){
+                    dfs(nbr,vis,n, isConnected);
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n= isConnected.size();
+        unordered_map<int,bool>vis;
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(vis[i]!=true){
+                dfs(i,vis,n,isConnected);
+                count++;
+            }
+        }
+        return count;
+    }
+
+};
