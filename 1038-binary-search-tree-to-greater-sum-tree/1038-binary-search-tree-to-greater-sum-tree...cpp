6cedd2c46dfcbd0e7478053ba069420a9a454480
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-10-26 23:17:01 +0530

    Time: 0 ms (100%), Space: 11 MB (9.65%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree...cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void storeInorder(TreeNode* root, vector<int>& inorder){
+        if(root==NULL) return;
+
+        //LNR
+        storeInorder(root->left, inorder);
+        inorder.push_back(root->val);
+        storeInorder(root->right, inorder);
+    }
+
+    void updateTree(TreeNode* root, vector<int> & inorder, int &index){
+        if(root == NULL) return;
+
+        //LNR
+        updateTree(root->left, inorder, index);
+        root->val = inorder[index];
+        index++;
+        updateTree(root->right, inorder, index);
+
+        return;
+    }
+
+    TreeNode* bstToGst(TreeNode* root) {
+        if(root == NULL) return root;
+        if(root->left == NULL && root->right ==NULL){
+            return root;
+        }
+
+        //step 1: store inorder
+        vector<int> inorder;
+        storeInorder(root, inorder);
+
+        //step2: update inorder
+        int n= inorder.size();
+        for(int i=n-1; i>=0; i--){
+            int currVal = inorder[i];
+            int nextVal = 0;
+            if(i+1 < n){
+                nextVal = inorder[i+1];
+            }
+            int sum = currVal + nextVal;
+            //updating the inorder vector
+            inorder[i] = sum;
+        }
+
+        //step3: update tree using inorder
+        int index = 0;
+        updateTree(root, inorder, index);
+        return root;
+    }
+};
