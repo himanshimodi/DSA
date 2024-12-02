e8595c95c80045a09eb87dbe50f0539c31a4c286
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-02 12:04:17 +0530

    Time: 18 ms (95.52%), Space: 139.2 MB (32.3%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -12,26 +12,26 @@ public:
             //include curr ele
             dq.push_back(i); //index he dal rhe hai queue mai
         }
+        //store ans
+        ans.push_back(nums[dq.front()]);
+
+        //remaining window ko process
+        for(int i=k; i<nums.size();i++){
+            //removal
+            if(!dq.empty() && i - dq.front() >= k){
+                dq.pop_front();
+            }
+
+            //addtion
+            while(!dq.empty() && nums[dq.back()]<nums[i]){
+            dq.pop_back();
+            }
+            //include curr ele
+            dq.push_back(i);
+
             //store ans
             ans.push_back(nums[dq.front()]);
-
-            //remaining window ko process
-            for(int i=k; i<nums.size();i++){
-                //removal
-                if(!dq.empty() && i - dq.front() >= k){
-                    dq.pop_front();
-                }
-
-                //addtion
-                while(!dq.empty() && nums[dq.back()]<nums[i]){
-                dq.pop_back();
-                }
-                //include curr ele
-                dq.push_back(i);
-
-                //store ans
-                ans.push_back(nums[dq.front()]);
-            }
+        }
         return ans;
     }
 };
