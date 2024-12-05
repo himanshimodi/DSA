deb14eae364314e2a5a783612a09337d5f6979f1
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-05 20:10:18 +0530

    Time: 3 ms (5.91%), Space: 10.6 MB (7.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -40,13 +40,43 @@ public:
         }
         return dp[0];
     }
+
+    int spaceOptimised(vector<int>& nums){
+        int n = nums.size();
+
+        //step 1
+        vector<int> dp( n, -1);
+
+        //step 2
+        int prev = nums[n-1];
+        int next = 0, curr;
+
+        for(int i =n-1; i>=0; i--){
+
+            int tempAns =0;
+            if(i+2 < n){
+                tempAns = next;
+            }
+            int include = nums[i] + tempAns;
+            int exclude =0 + prev;
+            curr = max(include, exclude);
+
+            //dont forget updating
+            next = prev;
+            prev = curr;
+        }
+        return prev;
+    }
     int rob(vector<int>& nums) {
         // int n = nums.size();
 
         //creating a dp
         // vector<int> dp(n, -1);
+
         // return solveUsingMemo(nums, 0, dp);
-        int ans = solveUsingTabulation(nums);
-        return ans;
+        // int ans = solveUsingTabulation(nums);
+        // return ans;
+
+        return spaceOptimised(nums);
     }
 };
