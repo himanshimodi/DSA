1e6a6198dcbb7d3f95c9b317e01bead2afa4ff04
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-07 23:54:45 +0530

    Time: 31 ms (13.32%), Space: 9.4 MB (80.76%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -52,25 +52,51 @@ public:
     }
 
     int solveUsingTab(int n, int k, int target) {
-    vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
 
-    dp[0][0] = 1;
+        dp[0][0] = 1;
 
-    for (int i = 1; i <= n; i++) {
-        for (int sum = 0; sum <= target; sum++) {
-            int ans = 0;
-            for (int face = 1; face <= k; face++) {
-                int recAns = 0;
-                if (sum - face >= 0) {
-                   recAns = dp[i - 1][sum - face];
+        for (int i = 1; i <= n; i++) {
+            for (int sum = 0; sum <= target; sum++) {
+                int ans = 0;
+                for (int face = 1; face <= k; face++) {
+                    int recAns = 0;
+                    if (sum - face >= 0) {
+                        recAns = dp[i - 1][sum - face];
+                    }
+                    ans = (ans % MOD + recAns % MOD) % MOD;
                 }
-                ans = (ans%MOD + recAns%MOD)%MOD;
+                dp[i][sum] = ans;
             }
-            dp[i][sum] = ans;
         }
+
+        return dp[n][target];
     }
 
-    return dp[n][target];
+    int solveUsingTabSO(int n, int k, int target) {
+
+        vector<int> curr(target + 1, 0);
+        vector<int> next(target + 1, 0);
+
+        curr[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int sum = 0; sum <= target; sum++) {
+                int ans = 0;
+                for (int face = 1; face <= k; face++) {
+                    int recAns = 0;
+                    if (sum - face >= 0) {
+                        recAns = curr[sum - face];
+                    }
+                    ans = (ans % MOD + recAns % MOD) % MOD;
+                }
+                next[sum] = ans;
+            }
+            // updating
+            curr = next;
+        }
+
+        return curr[target];
     }
 
     int numRollsToTarget(int n, int k, int target) {
@@ -81,7 +107,10 @@ public:
         // int ans = solveUsingMem(n, k, target, dp);
         // return ans;
 
-        int ans = solveUsingTab(n, k, target);
+        // int ans = solveUsingTab(n, k, target);
+        // return ans;
+
+        int ans = solveUsingTabSO(n, k, target);
         return ans;
     }
 };
