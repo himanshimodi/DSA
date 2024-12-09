997d0463afeace17d5ef99d9f3dd1ffe45cac3e1
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-09 17:04:53 +0530

    Time: 4 ms (63.02%), Space: 11.1 MB (73.58%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -52,6 +52,35 @@ public:
         return dp[i];
     }
 
+    bool solveTab(string &s, set<string>& wordSet){
+
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        //BC
+        dp[n] = true;
+
+        for(int index = n-1; index>=0; index --){
+            bool found = false;
+            string temp="";
+            //ek case
+            for(int j=index; j<n;j++){
+                temp += s[j];
+                //if temp exists in the set
+                found = wordSet.find(temp)!=wordSet.end();
+                if( found == true){
+                    //agar word mila
+                    if(dp[j+1] == true){
+                        dp[index] = true;
+                        found = true;
+                        break;
+                    }
+                }
+                dp[index] = false;
+            }
+        }
+        return dp[0];
+    }
+
     bool wordBreak(string s, vector<string>& wordDict) {
 
         int n = s.size();
@@ -60,8 +89,11 @@ public:
         // int ans = solveRE(s, wordSet,0);
         // return ans;
 
-        vector<int> dp(n+1,-1);
-        int ans = solveMem(s, wordSet,0,dp);
+        // vector<int> dp(n+1,-1);
+        // int ans = solveMem(s, wordSet,0,dp);
+        // return ans;
+
+        int ans = solveTab(s, wordSet);
         return ans;
     }
 };
