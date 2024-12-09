40fbebe6646ca83d36a7904cc3f26c7797e8cc58
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-10 00:39:00 +0530

    Time: 0 ms (100%), Space: 17.1 MB (94.88%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -39,7 +39,44 @@ public:
         return ans;
     }
 
+    vector<TreeNode*> solveMem(int n, int s, int e,vector<vector<vector<TreeNode*>>>&dp){
+        //BC
+        if(n<=1){
+            TreeNode* temp = new TreeNode(s);
+            return {temp};
+        }
+        if(s>e){
+            return {0};
+        }
+
+        //step 3
+        if(dp[s][e].size()!=0){
+            return dp[s][e];
+        }
+        
+        //ek case solve krdo
+        vector<TreeNode*>ans;
+        for(int i=s;i<=e;i++){
+            vector<TreeNode*>left = solveMem(n,s,i-1,dp);
+            vector<TreeNode*>right = solveMem(n,i+1,e,dp);
+            for(int j=0;j<left.size();j++){
+                for(int k=0;k<right.size();k++){
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left[j];
+                    root->right = right[k];
+                    ans.push_back(root);
+                }
+            }
+        }
+        dp[s][e] = ans;
+        return dp[s][e];
+    }
+
     vector<TreeNode*> generateTrees(int n) {
-        return solveRE(n,1,n);
+        // return solveRE(n,1,n);
+
+        vector<vector<vector<TreeNode*>>>dp(n+1,vector<vector<TreeNode*>>(n+1,vector<TreeNode*>(0)));
+        return solveMem(n,1,n,dp);
+
     }
 };
