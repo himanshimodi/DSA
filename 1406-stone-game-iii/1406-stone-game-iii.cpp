091f49d3e91afa622febb12fdc13e4c369544eb9
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-10 12:17:45 +0530

    Time: 22 ms (68%), Space: 136 MB (58.29%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -46,12 +46,37 @@ public:
         return dp[i];
     }
 
+    int solveTab(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int>dp(n+1,0);
+
+        dp[n] = 0;
+
+        for(int index = n-1;index>=0;index--){
+            // ek case solve krdo
+            int ans = INT_MIN;
+            int total = 0;
+            // X is number of stones
+            for (int X = 1; X <= 3; X++) {
+                if (index + X - 1 >= stoneValue.size()) {
+                    break;
+                }
+                total += stoneValue[index + X - 1];
+                ans = max(ans, total - dp[index+X]);
+            }
+            dp[index] = ans;
+        }
+        return dp[0];
+    }
+
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
         // int ans = solveRE(stoneValue, 0);
 
-        vector<int>dp(n+1,-1);
-        int ans = solveMem(stoneValue, 0,dp);
+        // vector<int>dp(n+1,-1);
+        // int ans = solveMem(stoneValue, 0,dp);
+
+        int ans = solveTab(stoneValue);
 
         if (ans > 0)
             return "Alice";
