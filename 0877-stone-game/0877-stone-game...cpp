9f19bd97026537ab9cc4237158cebc2c8368ea5c
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-10 12:33:15 +0530

    Time: 0 ms (100%), Space: 10.4 MB (53.91%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game...cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        //there will always be atleast one way by which Alice will win
+        //that means we should return true
+        //because the problem is asking for a possibility that Alice will win or not
+        return true;
+    }
+};
