166f0d439d3021273f5645e5307f24456972426c
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-18 17:07:58 +0530

    Time: 0 ms (100%), Space: 35.9 MB (49.97%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -60,8 +60,28 @@ public:
         return vector<int> (arr.begin() + l + 1, arr.begin() + h);
     }
 
+
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        //return twoPtrMethod(arr, k, x);
-        return bs_method(arr, k, x);
+
+        return bs_method(arr, k, x);      
+        
+        
+        // vector<int> ans;
+        // priority_queue<pair<int,int>> maxHeap;
+
+        // for(int i =0;i<arr.size();i++){
+        //     int diff = abs(arr[i]-x);
+        //     maxHeap.push({diff,arr[i]});
+        //     if(maxHeap.size()>k){
+        //         maxHeap.pop();
+        //     }
+        // }
+        // while(!maxHeap.empty() && k--){
+        //     int value = maxHeap.top().second;
+        //     maxHeap.pop();
+        //     ans.push_back(value);
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
     }
 };
