76b399859f710e8fd87d9f06bd435d57cae4d26a
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-22 18:58:33 +0530

    Time: 19 ms (23.62%), Space: 17.2 MB (12.43%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,49 +1,21 @@
 class Solution {
 public:
-    // string removeDuplicates(string s) {
-    //     string ans="";
-    //     int i=0;
-
-    //     while(i < s.length() ){
-
-    //         if(ans.length() > 0){
-    //              if( ans[ans.length()-1] == s[i] ) {
-    //                 ans.pop_back();
-    //             }
-    //             else{
-    //                 ans.push_back(s[i]);
-    //             }
-    //         }
-    //         else {
-    //             ans.push_back(s[i]);
-    //         }
-    //         i++;
-    //     }
-    //     return ans;
-    // }
-
-
-    //DONE USING STACK, THIS IS THE BEST POSSIBLE SOLUTION
-    string removeDuplicates(string s){
-        stack<char> st;
-        for( int i=0; i<s.length();i++){
-            if(!st.empty() && st.top() == s[i]){
-                //if character is same as the top of the stack
+    string removeDuplicates(string s) {
+        stack<int> st;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (!st.empty() && s[i] == st.top()) {
                 st.pop();
-            }
-            else{
-                //if character is not same as the top of the stack
+            } else {
                 st.push(s[i]);
             }
         }
-
-            string ans;
-            while(!st.empty()){
-                ans+= st.top();
-                st.pop();
-            }
-
-            reverse(ans.begin(), ans.end());
-            return ans;
+        string ans;
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
