50e8bc55d091af1bfeaa5b43bdf3ee97a162d903
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-23 05:28:30 +0530

    Time: 3 ms (61.85%), Space: 11.8 MB (34.8%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -5,14 +5,12 @@ public:
         stack<string> st;
         string res;
         
-        for(int i = 0;  i<path.size(); ++i)
-        {
+        for(int i = 0;  i<path.size(); ++i){
             if(path[i] == '/')    
                 continue;
             string temp;
 			// iterate till we encounter the last /
-            while(i < path.size() && path[i] != '/')
-            {
+            while(i < path.size() && path[i] != '/'){
 				// add path to temp string
                 temp += path[i];
                 ++i;
@@ -20,10 +18,10 @@ public:
             if(temp == ".")
                 continue;
 			// pop the top element from stack if exists
-            else if(temp == "..")
-            {
-                if(!st.empty())
+            else if(temp == ".."){
+                if(!st.empty()){
                     st.pop();
+                }
             }
             else
 			// push the directory file name to stack
@@ -31,16 +29,15 @@ public:
         }
         
 		// adding all the stack elements to res
-        while(!st.empty())
-        {
+        while(!st.empty()){
             res = "/" + st.top() + res;
             st.pop();
         }
         
 		// if no directory or file is present
-        if(res.size() == 0)
+        if(res.size() == 0){
             return "/";
-        
+        }        
         return res;
     }
 };
