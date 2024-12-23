6069928fd3e94534cd70ac0c3a1a00606b3520ff
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-23 19:44:48 +0530

    Time: 6 ms (47.66%), Space: 11 MB (40.14%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        stack<char>st;
         char ch;
         int i=0;
         while(i<s.size()){
             ch = s[i];
-
-            if(s[0] != 'a'){
+            if(s[0]!='a'){
                 return false;
             }
 
             if(ch == 'a'){
-                // if(st.empty()){
-                    st.push(ch);
-                // }
+                st.push(ch);
             }
-            else if(ch =='b'){
-                if(!st.empty() && st.top()=='a'){
+            else if(ch == 'b'){
+                if(!st.empty()&&st.top()=='a'){
                     st.push(ch);
                 }
                 else{
                     return false;
                 }
             }
-            if(ch =='c'){
-                if(!st.empty() && st.top()=='b'){
+            if(ch == 'c'){
+                if(!st.empty()&&st.top()=='b'){
                     st.pop();
-                    if(!st.empty() && st.top()=='a'){
+                    if(!st.empty()&&st.top()=='a'){
                         st.pop();
                     }
                     else{
                         return false;
                     }
-                    
                 }
                 else{
                     return false;
@@ -41,8 +37,7 @@ public:
             }
             i++;
         }
-
-        if(st.empty()){
+        if(st.empty() == true){
             return true;
         }
         else{
