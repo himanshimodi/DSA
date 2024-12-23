1601c8adafba457f24439681f1ae64ba26a3d747
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-12-23 19:44:54 +0530

    Time: 6 ms (47.66%), Space: 11 MB (40.14%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutionsRevision1.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        char ch;
+        int i=0;
+        while(i<s.size()){
+            ch = s[i];
+            if(s[0]!='a'){
+                return false;
+            }
+
+            if(ch == 'a'){
+                st.push(ch);
+            }
+            else if(ch == 'b'){
+                if(!st.empty()&&st.top()=='a'){
+                    st.push(ch);
+                }
+                else{
+                    return false;
+                }
+            }
+            if(ch == 'c'){
+                if(!st.empty()&&st.top()=='b'){
+                    st.pop();
+                    if(!st.empty()&&st.top()=='a'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+            i++;
+        }
+        if(st.empty() == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
