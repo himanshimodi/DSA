d4d2d50251b298de6b95a3653f964760535c6dbc
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2025-02-16 19:00:49 +0530

    Time: 108 ms (5.01%), Space: 141.7 MB (6%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,111 @@
+class TrieNode{
+    public: 
+        char data;
+        unordered_map<char, TrieNode*> children;
+        bool isTerminal;
+
+        TrieNode(char val ) {
+            data = val;
+            isTerminal = false;
+        }
+};
+
+void insertWord(TrieNode* root, string word) {
+            //base case 
+            if(word.length() == 0) {
+                root->isTerminal = true;
+                return;
+            }
+
+            //1 case hum khud solve krenge 
+            char ch = word[0];
+            TrieNode* child ;
+            if(root->children.find(ch) != root->children.end()) {
+                //present
+                child = root->children[ch];
+            }
+            else {
+                //absent
+                child = new TrieNode(ch);
+                root->children[ch] = child;
+            }
+            //baaki recursion
+            insertWord(child, word.substr(1));
+}
+
+bool searchWord(TrieNode* root, string word) {
+    //base case
+    if(word.length() == 0) {
+        return root->isTerminal;
+    }
+
+    //1 case solve krdo
+    char ch = word[0];
+    TrieNode* child;
+    if(root->children.find(ch) != root->children.end()) {
+        //present
+        child = root->children[ch];
+    }
+    else {
+        //absent
+        return false;
+    }
+    //baaki recursion sambhal lega
+    bool recKaAns = searchWord(child, word.substr(1));
+    return recKaAns;
+}
+
+bool searchPrefix(TrieNode* root, string word) {
+    //base case
+    if(word.length() == 0) {
+        return true;
+    }
+
+    //1 case solve krdo
+    char ch = word[0];
+    TrieNode* child;
+    if(root->children.find(ch) != root->children.end()) {
+        //present
+        child = root->children[ch];
+    }
+    else {
+        //absent
+        return false;
+    }
+    //baaki recursion 
+    bool recKaAns = searchPrefix(child, word.substr(1));
+    return recKaAns;
+}
+
+//my class - TreiNode
+//class given by leetcode - Trie
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode('-');
+    }
+
+    void insert(string word) {
+       insertWord(root, word);
+    }
+    
+    bool search(string word) {
+        bool ans = searchWord(root, word);
+        return ans;
+    }
+    
+    bool startsWith(string prefix) {
+        bool ans = searchPrefix(root, prefix);
+        return ans;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
