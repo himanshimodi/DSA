2a8560ee4e396f7bcbf513bac77ae5a990d34d00
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2025-02-19 15:28:52 +0530

    Time: 117 ms (51.73%), Space: 105.3 MB (44.42%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.cpp
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.cpp
@@ -1,19 +1,15 @@
-class TrieNode
-{
+class TrieNode {
 public:
     char data;
-    unordered_map<char, TrieNode *> children;
+    unordered_map<char, TrieNode*> children;
     bool isTerminal;
     TrieNode(char ch) : data(ch), isTerminal(false) {}
 };
 
-class Trie
-{
-    TrieNode *root;
-    void insertUtil(TrieNode *root, string &word, int i = 0)
-    {
-        if (i == word.size())
-        {
+class Trie {
+    TrieNode* root;
+    void insertUtil(TrieNode* root, string& word, int i = 0) {
+        if (i == word.size()) {
             root->isTerminal = true;
             return;
         }
@@ -23,24 +19,21 @@ class Trie
         insertUtil(root->children[word[i]], word, i + 1);
     }
 
-    bool searchUtil(TrieNode *root, string &word, int i = 0, bool mismatch = false)
-    {
+    bool searchUtil(TrieNode* root, string& word, int i = 0,
+                    bool mismatch = false) {
         if (i == word.size())
             return mismatch && root->isTerminal;
 
         auto ch = word[i];
-        if (root->children.find(ch) != root->children.end())
-        {
+        if (root->children.find(ch) != root->children.end()) {
             // match till you can
             if (searchUtil(root->children[ch], word, i + 1, mismatch))
                 return true;
         }
 
         // if you are here means, there is a mismatch
-        if (!mismatch)
-        {
-            for (auto &[childChar, childNode] : root->children)
-            {
+        if (!mismatch) {
+            for (auto& [childChar, childNode] : root->children) {
                 if (ch != childChar && searchUtil(childNode, word, i + 1, true))
                     return true;
             }
@@ -49,41 +42,25 @@ class Trie
     }
 
 public:
-    Trie()
-    {
-        root = new TrieNode('\0');
-    }
+    Trie() { root = new TrieNode('\0'); }
 
-    void insert(string &word)
-    {
-        insertUtil(root, word);
-    }
+    void insert(string& word) { insertUtil(root, word); }
 
-    bool search(string &word)
-    {
-        return searchUtil(root, word);
-    }
+    bool search(string& word) { return searchUtil(root, word); }
 };
 
-class MagicDictionary
-{
+class MagicDictionary {
     Trie trie;
 
 public:
-    MagicDictionary()
-    {
-    }
+    MagicDictionary() {}
 
-    void buildDict(vector<string> dictionary)
-    {
-        for (auto &word : dictionary)
+    void buildDict(vector<string> dictionary) {
+        for (auto& word : dictionary)
             trie.insert(word);
     }
 
-    bool search(string searchWord)
-    {
-        return trie.search(searchWord);
-    }
+    bool search(string searchWord) { return trie.search(searchWord); }
 };
 
 /**
