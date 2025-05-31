1b37adc1102aae9d89c967a1f2053f9704b014fa
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2025-05-31 18:37:46 +0530

    Time: 4 ms (17.25%), Space: 7.5 MB (100%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string newstring;
-        for(char str : s){
-            if(isalpha(str) || isdigit(str)){
-                newstring+=tolower(str);
+        int start=0;
+        int end=s.size()-1;
+        while(start<=end){
+            if(!isalnum(s[start])){ start++; continue;}
+            if(!isalnum(s[end])){end--; continue;}
+            if(tolower(s[start])!=tolower(s[end])){
+               return false;
             }
             else{
-                continue;
-            }
-        }
-        int start = 0;
-        int end = newstring.size()-1;
-        while(start<=end){
-            if(newstring[start]==newstring[end]){
                 start++;
                 end--;
-            }
-            else{
-                return false;
             }
         }
         return true;
