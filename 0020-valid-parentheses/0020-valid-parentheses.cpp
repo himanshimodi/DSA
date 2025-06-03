2051b4a1746ac73209c6350954243803d620b6ed
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2025-06-03 07:20:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (61.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s[i];
-
-            // opening bracket
-            if (ch == '(' || ch == '{' || ch == '[') {
+        stack<char>st;
+        for(char ch : s){
+            if(ch=='('|| ch=='{'|| ch=='['){
                 st.push(ch);
-            } else {
-                // closing bracket (ch is of input string)
-                if (!st.empty()) {
-                    char topCh = st.top();
-
-                    // matching brackets
-                    if (ch == ')' && topCh == '(') {
+            }
+            else{
+                //closing bracket
+                if(!st.empty()){
+                    if(ch==')' && st.top()=='('){
+                    st.pop();
+                    }
+                    else if(ch=='}' && st.top()=='{'){
                         st.pop();
-                    } else if (ch == '}' && topCh == '{') {
+                    }
+                    else if(ch==']' && st.top()=='['){
                         st.pop();
-                    } else if (ch == ']' && topCh == '[') {
-                        st.pop();
-                    } else {
+                    }
+                    else{
                         return false;
                     }
                 }
@@ -30,10 +27,11 @@ public:
                 }
             }
         }
-        if (st.empty()) {
-            // valid
+
+        if(st.empty()){
             return true;
-        } else {
+        }
+        else{
             return false;
         }
     }
