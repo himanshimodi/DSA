d7bcf4ca7b520a0f8999889e8684a783c4864817
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2025-06-15 12:07:51 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.89%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum..cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum..cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-
+    // SIMILAR SUMS ARE PAINTER'S PARTITION PROBLEM ON GFG AND 
+    // ALLOCATE MINIMUM BOOK PROBLEM ON GFG
+    // AGGRESSIVE COWS ON GFG IS SIMILAR BUT DIFFERENT, PLEASE REFER
     bool isPossible(vector<int>& nums, int k,int mid){
         int subarray=1;
         int arrelement=0;
