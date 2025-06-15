281a79c77acf1c4a8f22da8b54dce34515ac6f44
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2025-06-15 17:10:40 +0530

    Time: 74 ms (78.15%), Space: 146.5 MB (22.41%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -4,22 +4,24 @@ public:
         if(nums.size()<2){
             return false;
         }
-        unordered_map<int,int>map;
+
+        unordered_map<int,int>map; //{sum,index}
         map[0]=-1;
-        int sum =0;
+        int sum=0;
 
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             if(k!=0){
-                sum = sum %k;
+                sum = sum % k;
             }
-            if(map.find(sum)!= map.end()){
-                //present in map
-                if(i-map[sum]>1)
-                return true;
+            if(map.find(sum)!=map.end()){ //sum is already present in the map
+                if(i-map[sum]>=2){
+                    //len of subarray should be more than or equal to 2
+                    return true;
+                }
             }
             else{
-                //push it in the map
+                //sum is not already present toh sum ko map mai dal do
                 map[sum]=i;
             }
         }
