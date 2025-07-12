63e528cef01f361f7e1a06271966a60e94eeaab6
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2025-07-12 17:19:17 +0530

    Time: 151 ms (35.15%), Space: 111.1 MB (22.83%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -44,9 +44,9 @@ public:
         return dp[i][sum];
     }
 
-    bool canPartition(vector<int>& nums) {
+    bool tabulation(vector<int>&nums){
         int n = nums.size();
-        int sum, target;
+        int sum=0, target=0;
         for(int i : nums){
             sum+=i;
         }
@@ -55,10 +55,50 @@ public:
         if(sum%2 == 1){
             return false;
         }
+        //step 1
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+
+        //step 2: analyze base cases
+        for(int temp =0;temp<=target;temp++){
+            dp[n][temp]=0;
+        }
+        for(int temp =0;temp<=n;temp++){
+            dp[temp][target]= 1;
+        }
+        int inc =0, exc =0;
+
+        for(int i=n-1;i>=0;i--){
+            for(int currSum=target-1;currSum>=0;currSum--){
+                //ek case
+                if(currSum + nums[i]<=target){
+                    inc = dp[i+1][currSum+nums[i]];
+                }
+                
+                exc = dp[i+1][currSum];
+                dp[i][currSum]= (inc||exc);
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        // int sum, target;
+        // for(int i : nums){
+        //     sum+=i;
+        // }
+        // target =sum /2;
+
+        // if(sum%2 == 1){
+        //     return false;
+        // }
         // return solveRE(0, 0, target, nums);
         
         //step 1
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        return memo(0,0,target,nums,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
+        // return memo(0,0,target,nums,dp);
+
+        return tabulation(nums);
     }
 };
